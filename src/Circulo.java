40da1763d7c0d78d40528273c99813df3bd0200b
nachomartinez <nachonmg@outlook.es>
2022-01-18 17:42:28 +0100

    DNI

---
+++ src/Circulo.java
@@ -0,0 +1,24 @@
+public class Circulo {
+ private double centroX;
+ private double centroY;
+ private double radio;
+ public Circulo(
+ double cx, double cy, double r) {
+ centroX = cx;
+ centroY = cy;
+ radio = r;
+ }
+ public double getCentroX() {
+ return centroX;
+ }
+ public double getCircunferencia() {
+ return 2 * Math.PI * radio;
+ }
+ public void mueve(double deltaX, double deltaY) {
+ centroX = centroX + deltaX;
+ centroY = centroY + deltaY;
+ }
+ public void escala(double s) {
+ radio = radio * s;
+ }
+}
