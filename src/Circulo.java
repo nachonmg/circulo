44dc52ef89a645e4006fe07f20d5a4ba6d6b1c90
nachomartinez <nachonmg@outlook.es>
2022-01-18 18:02:51 +0100

    se ha modificado circulo

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,3 +1,11 @@
+/**
+ * Clase Circulo con radio y centro
+ * @author Nacho
+ * @version 1.0
+ */
+
+
+
 public class Circulo {
  private double centroX;
  private double centroY;
@@ -8,16 +16,34 @@ public class Circulo {
  centroY = cy;
  radio = r;
  }
+ 
+ /**
+  * Calcula el centro del Circulo
+  * @return Devuelve el centro del Circulo
+  */
  public double getCentroX() {
  return centroX;
  }
+ /**
+  * Calcula el centro del Circulo
+  * @return Devuelve el centro del Circulo
+  */
  public double getCircunferencia() {
- return 2.1 * Math.PI * radio;
+ return 2 * Math.PI * radio;
  }
+ /**
+  * Calcula el centro del Circulo
+  * @param deltaX devuelve deltaX
+  * @param deltaY devuelve deltaY
+  */
  public void mueve(double deltaX, double deltaY) {
  centroX = centroX + deltaX;
  centroY = centroY + deltaY;
  }
+ /**
+  * Escala del Circulo
+  * @param s  multiplica el circulo
+  */
  public void escala(double s) {
  radio = radio * s;
  }
